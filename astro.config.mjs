82203cb7273bba59d77918ad71029cb382fbaaf8
Jestercorps <russelraaths@live.com>
2026-06-18 18:23:59 +0200

    Fix unresolved merge markers in homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
+<<<<<<< HEAD
 import sitemap from '@astrojs/sitemap';
+=======
+<<<<<<< HEAD
+=======
+import sitemap from '@astrojs/sitemap';
+>>>>>>> 91112b1 (Fix unresolved merge markers in homepage)
+>>>>>>> c883f02 (Fix unresolved merge markers in homepage)
 
 export default defineConfig({
   site: 'https://russelraaths.github.io',
   base: '/EveryNationTzaneenAstro',
+<<<<<<< HEAD
   integrations: [sitemap()],
+=======
+<<<<<<< HEAD
+=======
+  integrations: [sitemap()],
+>>>>>>> 91112b1 (Fix unresolved merge markers in homepage)
+>>>>>>> c883f02 (Fix unresolved merge markers in homepage)
 });
