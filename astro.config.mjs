1bc0f6a4a75fe5615022e4a8ebae7b202fccfd0b
Jestercorps <russelraaths@live.com>
2026-06-18 17:15:07 +0200

    Fix Pages deployment and add lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+<<<<<<< HEAD
 
 export default defineConfig({
   site: 'https://russelraaths.github.io',
   base: '/EveryNationTzaneenAstro',
+=======
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: "https://russelraaths.github.io",
+  base: "/EveryNationTzaneenAstro",
+>>>>>>> 9688831 (Fix Pages deployment and add lockfile)
 });
