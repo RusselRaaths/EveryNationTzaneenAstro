e2df767679339e82cea7a81fb08dce595992f0a3
RusselRaaths <92252389+RusselRaaths@users.noreply.github.com>
2026-09-12 08:00:51 +0200

    Remove Tailwind integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // or whatever integration you use
 
 export default defineConfig({
   site: 'https://entzaneen.co.za',
   base: '/ChurchWebsite',
-  integrations: [tailwind()],
 });
