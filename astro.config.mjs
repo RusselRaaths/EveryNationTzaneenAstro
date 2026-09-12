817afc7e549fdb929c820197189effe895b6dc5d
Jestercorps <russelraaths@live.com>
2026-09-12 08:20:14 +0200

    Production build for deployment to https://entzaneen.co.za/ChurchWebsite/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://russelraaths.github.io',
-  base: '/EveryNationTzaneenAstro',
-  integrations: [tailwind(), sitemap()],
+  site: 'https://entzaneen.co.za',
+  base: '/ChurchWebsite/',
 });
